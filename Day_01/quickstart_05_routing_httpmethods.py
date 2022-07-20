3121d2c8956b48bc539b72b947e8495d87458b5b
felixyh <felixyanghui@gmail.com>
2022-07-20 11:57:52 +0800

    update

--- Day_01/quickstart_05_routing_httpmethods.py
+++ Day_01/quickstart_05_routing_httpmethods.py
@@ -4,6 +4,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# Method-1:By default, a route only answers to GET requests.
+# You can use the methods argument of the route() decorator to handle different HTTP methods.
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -11,3 +15,14 @@ def login():
     else:
         return show_the_login_form()
 
+
+# Method-2: separate views for different methods into different functions.
+# Flask provides a shortcut for decorating such routes with get(), post(), etc. for each common HTTP method.
+@app.get('/login')
+def get_login():
+    return show_the_login_form()
+
+
+@app.post('/login')
+def post_login():
+    return do_log_login()
--- apply.py
+++ apply.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,22 +8,14 @@ def hello(name):
     return f"Hello, {escape(name)}!"
 
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
+@app.route('/')
+def index():
+    return 'hello, index!!'
 
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
