5caaeadc7ec0dbaff48690ef71c1f13191cccfc9
felixyh <felix_yanghui@sina.cn>
2022-07-21 17:35:37 +0800

    update

---
+++ Day_01/quickstart_09_ARD_request.py
@@ -0,0 +1,23 @@
+from flask import request, Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        # if valid_login(request.form['username'],
+        #                request.form['password']):
+        #     return log_the_user_in(request.form['username'])
+        # else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
